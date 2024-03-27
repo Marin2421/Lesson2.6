815cc6981a0ebf0b4e9266a4b0fe27e9f801b17a
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-03-27 13:41:14 +0300

    Добавьте файлы проекта.

---
+++ Lesson2.6/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = 10;
+Console.WriteLine(num.GetType());
+
+Double doubleNum = 10.5;
+string doubleStr = doubleNum.ToString();
+Console.WriteLine(doubleStr);
+
+string numStr = "25";
+int parsedNum = int.Parse(numStr);
+Console.WriteLine(parsedNum);
